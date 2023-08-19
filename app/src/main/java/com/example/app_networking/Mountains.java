9c86ed9c62c51bf8499e7fb261b1196df7756f82
a21malfo <a21@malfo@student.his.se>
2023-08-19 19:05:22 +0200

    Add Mountains.java and create to get name.

---
+++ app/src/main/java/com/example/app_networking/Mountains.java
@@ -0,0 +1,17 @@
+package com.example.app_networking;
+
+public class Mountains {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Mountains (String name) {
+        this.name = name;
+    }
+}
+
